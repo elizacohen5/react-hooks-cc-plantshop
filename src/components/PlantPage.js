ad31e8d1070609575df71b157789706f8c1ebcce
Eliza Cohen <elizacohen5@gmail.com>
2024-05-23 10:46:58 -0400

    complete plantshop requirements

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
+import PlantCard from "./PlantCard";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plantData) => setPlants(plantData));
+  }, []);
+
+  function handleSearchChange(event) {
+    const plantSearch = event.target.value;
+    setSearch(plantSearch);
+    console.log(search);
+  }
+
+  const plantsToDisplay = plants.filter((plant) => {
+    const lowerCaseSearch = search.toLocaleLowerCase();
+    const lowerCasePlant = plant.name.toLocaleLowerCase();
+    return lowerCasePlant.includes(lowerCaseSearch);
+  });
+
+  const plantCardList = plantsToDisplay.map((plant) => {
+    return (
+      <PlantCard
+        key={plant.id}
+        name={plant.name}
+        price={plant.price}
+        image={plant.image}
+      />
+    );
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants} />
+      <Search onSearchChange={handleSearchChange} />
+      <PlantList plantCardList={plantCardList} />
     </main>
   );
 }
